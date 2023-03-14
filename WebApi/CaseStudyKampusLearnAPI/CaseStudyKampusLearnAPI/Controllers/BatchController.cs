b7f2e17a5a694046b9f129eb0ab9d588373752c2
Soundaria <78199061+Soundaria@users.noreply.github.com>
2023-03-14 12:39:12 +0530

    Initial Commit
    Kampus Learn

---
+++ WebApi/CaseStudyKampusLearnAPI/CaseStudyKampusLearnAPI/Controllers/BatchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaseStudyKampusLearnAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BatchController : ControllerBase
+	{
+
+	}
+}
---
+++ WebApi/CaseStudyKampusLearnAPI/CaseStudyKampusLearnAPI/Controllers/TrainerCourseController.cs
@@ -0,0 +1,60 @@
+﻿using CaseStudyKampusLearnAPI.Models;
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+
+namespace CaseStudyKampusLearnAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TrainerCourseController : ControllerBase
+	{
+
+		private readonly KampusLearnContext repo;
+		private readonly ILogger<Admin> logger;
+		public TrainerCourseController(KampusLearnContext repo, ILogger<Admin> logger)
+		{
+			this.repo = repo;
+			this.logger = logger;
+		}
+
+		//Getting Course by Trainer id
+		[HttpGet("GetCourseOfTrainer/{trainerId}")]
+		public IActionResult GetCourseOfTrainer(int trainerId)
+		{
+			try
+			{
+				List<TrainerCourse> trainercourse = repo.TrainerCourse.ToList();
+				List<Trainer> trainer = repo.Trainer.ToList();
+				List<Admin> admin = repo.Admin.ToList();
+				List<Course> course = repo.Course.ToList();
+				var id = trainercourse.FindAll(x => x.TrainerId == trainerId);
+				if (id != null)
+				{
+					logger.LogInformation("Course added to trainers and the details are listed");
+					return StatusCode(200, id);
+				}
+				else
+				{
+					logger.LogWarning("no trainer with courses found");
+					return StatusCode(404, "Course not found");
+				}
+			}
+			catch (NullReferenceException ex)
+			{
+				logger.LogWarning("Data not found" + ex.Message);
+				return StatusCode(404, "Data Not Found");
+			}
+			catch (Exception ex)
+			{
+				logger.LogWarning("Contact to Admin.. Server Error" + ex.Message);
+				return StatusCode(500, "Internal Server Error");
+			}
+
+		}
+
+	}
+}
